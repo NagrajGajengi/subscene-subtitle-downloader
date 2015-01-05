82b02f94330505ad1cfbf85e02b9d085eb87d195
NagrajGajengi <simplythenagraj@gmail.com>
2015-01-06 04:13:49 +0530

    Update getSubsceneSubs.cmd

--- getSubsceneSubs.cmd
+++ getSubsceneSubs.cmd
@@ -3,7 +3,7 @@ cls
 set PATH=%PATH%;C:\Python27\
 :my_loop
 IF %1=="" GOTO completed
-  python C:\Python27\work\Subscene-subtitle\subsceneSub.py %1
+  python C:\subsceneSub.py %1
   SHIFT
   GOTO my_loop
 :completed
